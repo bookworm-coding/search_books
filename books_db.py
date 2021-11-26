25d9fb4779ff227e1d6b2d350508858e71c36b4e
Users <bookwormkkm00@gmail.com>
2021-11-26 15:25:18 +0900

    commit

--- books_db.py
+++ books_db.py
@@ -1,5 +1,12 @@
 from sqlite3 import *
-def get():
+def get(column='*', where=''):
     conn = connect('books.db')
     c = conn.cursor()
-    
+    if where=='':
+        c.execute('SELECT ' + column + ' FROM books')
+    else:
+        c.execute('SELECT ' + column + ' FROM books WHERE ' + where)
+    a=c.fetchall()
+    conn.close()
+    return a
+def insert(isbn, place, title):
