1c4e9a4df1d526de6421c6f0d756ed13558f6d8b
Users <bookwormkkm00@gmail.com>
2021-12-02 15:50:17 +0900

    commit

--- main.py
+++ main.py
@@ -4,35 +4,41 @@ import log
 import scan
 import functions
 from log import command as com
+
 root: Tk = None
 window: Tk = None
 entry: Entry = None
 entry2: Entry = None
+
+
 def find():
     global window, root, entry, entry2
-    window=Tk()
-    entry=Entry(window, font=('Malgun Gothic', 20, "roman"))
+    window = Tk()
+    entry = Entry(window, font=('Malgun Gothic', 20, "roman"))
     entry.grid(row=0, column=1)
-    l=Label(window, text="찾으려는 책의 제목을 입력하세요.")
+    l = Label(window, text="찾으려는 책의 제목을 입력하세요.")
     l.grid(row=0, column=0)
+
     def next_com(*args, **kwargs):
         global window
-        data=entry.get()
+        data = entry.get()
         window.destroy()
-        search:list=functions.search_db(data)
-        if search == [] or search==None:
-            window=Tk()
-            l=Label(window, text="찾으려는 책이 없습니다.")
+        search: list = functions.search_db(data)
+        if search == [] or search == None:
+            window = Tk()
+            l = Label(window, text="찾으려는 책이 없습니다.")
             l.pack()
             window.mainloop()
         else:
-            window=Tk()
+            window = Tk()
 
             for i, j in search, range(0, len(search)):
-                l=Label(window, text=i[2]+"라는 책이 "+i[1]+"에 있습니다.\n")
+                l = Label(window, text=str(i[2]) + "라는 책이 " + str(i[1]) + "에 있습니다.")
                 l.pack()
             window.mainloop()
+
     window.bind("<Return>", next_com)
+
 
 def add():
     global window, entry
@@ -74,10 +80,10 @@ def start():
     root.Title = "도서검색 프로그램"
     a = Button(root, text="신규 등록", command=add, font=('Malgun Gothic', 20, "roman"))
     a.grid(row=0, column=1)
-    #a = Button(root, text="기존 책 뽑기", command=out, font=('Malgun Gothic', 20, "roman"))
-    #a.grid(row=0, column=2)
-    #a = Button(root, text="기존 책 꼽기", command=into, font=('Malgun Gothic', 20, "roman"))
-    #a.grid(row=0, column=3)
+    # a = Button(root, text="기존 책 뽑기", command=out, font=('Malgun Gothic', 20, "roman"))
+    # a.grid(row=0, column=2)
+    # a = Button(root, text="기존 책 꼽기", command=into, font=('Malgun Gothic', 20, "roman"))
+    # a.grid(row=0, column=3)
     a = Button(root, text="책 검색", command=find, font=('Malgun Gothic', 20, "roman"))
     a.grid(row=0, column=4)
     root.mainloop()
@@ -92,10 +98,10 @@ if __name__ == "__main__":
     root.Title = "도서검색 프로그램"
     a = Button(root, text="신규 등록", command=add, font=('Malgun Gothic', 20, "roman"))
     a.grid(row=0, column=1)
-    #a = Button(root, text="기존 책 뽑기"""", command=out""", font=('Malgun Gothic', 20, "roman"))
-    #a.grid(row=0, column=2)
-    #a = Button(root, text="기존 책 꼽기"""", command=into""", font=('Malgun Gothic', 20, "roman"))
-    #a.grid(row=0, column=3)
+    # a = Button(root, text="기존 책 뽑기"""", command=out""", font=('Malgun Gothic', 20, "roman"))
+    # a.grid(row=0, column=2)
+    # a = Button(root, text="기존 책 꼽기"""", command=into""", font=('Malgun Gothic', 20, "roman"))
+    # a.grid(row=0, column=3)
     a = Button(root, text="책 검색", command=find, font=('Malgun Gothic', 20, "roman"))
     a.grid(row=0, column=4)
     root.mainloop()
