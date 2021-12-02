95a9104a9aab2205472a8c89bd03d937396776a2
Users <bookwormkkm00@gmail.com>
2021-12-02 15:22:49 +0900

    commit

--- books_db.py
+++ books_db.py
@@ -1,19 +1,27 @@
 from sqlite3 import *
+
+
 def get(column='*', where=''):
     conn = connect('books.db')
     c = conn.cursor()
-    if where=='':
+    if where == '':
         c.execute('SELECT ' + column + ' FROM books')
     else:
         c.execute('SELECT ' + column + ' FROM books WHERE ' + where)
-    a=c.fetchall()
+    a = c.fetchall()
     conn.close()
     return a
+
+
 def insert(isbn, place, title):
-    conn=connect('books.db')
+    conn = connect('books.db')
     c = conn.cursor()
     c.execute("INSERT INTO books VALUES (:isbn, :place, :title)",
-              {"isbn": isbn, "place":place, "title":title})
+              {"isbn": isbn, "place": place, "title": title})
     conn.commit()
     conn.close()
     return
+
+
+if __name__ == "__main__":
+    insert(0, 0, 0)
